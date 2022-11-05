d6637712fc6d41eee9d3c50cf158f7c788c30061
Pedro Uga <pedrovictorcoelhouga@gmail.com>
2022-11-05 12:15:17 -0300

    enviar

--- imc.c
+++ imc.c
@@ -15,36 +15,28 @@ int main(int argc, char** argv){
 
     imc = (peso)/(altura*altura);
 
-    if(imc<16)
-    {
+    if(imc<16){
         printf("%0.2f Perigo de vida\n", imc);
     }
-    if(imc>=16 && imc<17)
-    {
+    if(imc>=16 && imc<17){
         printf("%0.2f Muito abaixo do peso\n", imc);
     }
-    if (imc>=17 && imc<18,5)
-    {
+    if (imc>=17 && imc<18,5){
         printf("%0.2f Abaixo do peso\n", imc);
     }
-    if (imc>=18,5 && imc<25)
-    {
+    if (imc>=18,5 && imc<25){
         printf("%0.2f Peso normal\n", imc);
     }
-    if (imc>=25 && imc<30)
-    {
+    if (imc>=25 && imc<30){
         printf("%0.2f Acima do peso\n", imc);
     }
-    if (imc>=30 && imc<35)
-    {
+    if (imc>=30 && imc<35){
         printf("%0.2f Obesidade grau I\n", imc);
     }
-    if (imc>=35 && imc<40)
-    {
+    if (imc>=35 && imc<40){
         printf("%0.2f Obesidade grau II\n", imc);
     }
-    if (imc>=40)
-    {
+    if (imc>=40){
         printf("%0.2f Obesidade grau III\n", imc);
     }
 return 0;
