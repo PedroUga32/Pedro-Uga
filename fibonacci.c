bf31439bb27b0257691cb8c1d0ac7531abc9d3c8
Pedro Uga <pedrovictorcoelhouga@gmail.com>
2022-11-05 14:47:45 -0300

    enviar

---
+++ fibonacci.c
@@ -0,0 +1,13 @@
+/*
+* Arquivo: fibonacci.c
+* Data Criação: 03/nov/2022
+* Autor: Pedro Uga 
+* Lab 1
+*/
+#include <stdio.h>
+#include <stdint.h>
+
+int main(int argc, char** argv){
+
+    return 0;
+}
