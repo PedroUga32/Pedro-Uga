bd0f1aab56631b20daf5c5f6597b41c50025fb3b
Pedro Uga <pedrovictorcoelhouga@gmail.com>
2022-11-05 11:17:37 -0300

    enviar

---
+++ .vscode/teste.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() 
+{
+    printf("Ola mundo ");
+    return 0;
+}
---
+++ PrimeiroCodigo.c
@@ -0,0 +1,13 @@
+/*
+* Arquivo: base.c
+* Data Criação: 27/out/2022
+* Autor: Pedro Uga 
+* Primeiro Programa em C
+*/
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    printf("Bom dia meu querido mestre!\n");
+    return 0;
+}
+    
