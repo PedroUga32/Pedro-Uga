d7065fa2f2d9ea4e3583d99f81ab2c4c68d6aa19
Pedro Uga <pedrovictorcoelhouga@gmail.com>
2022-11-06 11:43:15 -0300

    enviar

--- fibonacci.c
+++ fibonacci.c
@@ -7,7 +7,22 @@
 #include <stdio.h>
 #include <stdint.h>
 
-int main(int argc, char** argv){
+int fib(int n)
+{
+  int i, fib1 = 1, fib2 = 1, soma;
+  for (i = 3; i <= n; i = i + 1)
+  {
+    soma = fib1 + fib2;
+    fib1 = fib2;
+    fib2 = soma;
+  }
+  return fib2;
+}
 
-    return 0;
+int main(void)
+{
+  int n;
+  scanf("%d", &n);
+  printf("%d\n", fib(n));
+  return 0;
 }
