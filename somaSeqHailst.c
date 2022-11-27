1a06a073d516ae86f69ff31cc84981da16754ea8
Pedro Uga <pedrovictorcoelhouga@gmail.com>
2022-11-26 22:21:49 -0300

    enviar

--- somaSeqHailst.c
+++ somaSeqHailst.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void){
-    int numero;
-    printf("Escolha um número para iniciar a sequência de Hailstone: ")
-    scanf("%d", &numero);
-    
+int Hailstone (int n)
+{
+  if (n % 2 == 0) {
+    return n /= 2;
+  }
+  else {
+    return n = (3 * n) + 1;
+  }
+  return n;
+}
 
+int main (void)
+{
+  int numero, soma;
+
+  printf("Escolha um número para iniciar a sequência de Hailstone: ");
+  scanf("%d", &numero);
+  soma = Hailstone(numero);
+
+  printf("%d", soma);
+
+  return 0;
 }
