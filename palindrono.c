76437cfaca8967068528b81c763d40386e30b0f1
Pedro Uga <pedrovictorcoelhouga@gmail.com>
2022-12-10 18:44:20 -0300

    enviar

---
+++ palindrono.c
@@ -0,0 +1,18 @@
+/*
+* Arquivo: bin2dec.c
+* Data Criação: 08/dez/2022
+* Autor: Pedro Uga 
+* Lab 4
+*/
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main(){
+
+
+
+
+    return 0;
+}
