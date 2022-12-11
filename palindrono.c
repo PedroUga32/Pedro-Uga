52bf75fde86499074d2e8298dcb9e5a4c8a09930
Pedro Uga <pedrovictorcoelhouga@gmail.com>
2022-12-11 14:38:34 -0300

    enviar

--- palindrono.c
+++ palindrono.c
@@ -9,26 +9,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 int main(){
 
     char palavra[50], copia[50];
     int i, tam, diferentes = 0, j = 0;
 
     printf("Digite uma frase: ");
-    fgets("%50[^]", palavra);
+    fgets("50[^]", palavra);
 
-    for(i = 0; i < strlen(palavra); i++){
+    for(i = 0; i <= strlen(palavra); i++){
         if(palavra[i] != '!' && palavra[i] != '?' && palavra[i] != ' ' && palavra[i] != '.')
             copia[j++] = palavra[i];
     }
 
     copia[j] = '\0';
-
-    printf("Original: %s\nCopia:%s\n", palavra, copia);
 
     tam = strlen(copia);
     tam--;
